8d20cf2613c87c8c782d93946f3cdb1c012681fd
munyi-victor <munyivictor6@gmail.com>
2024-02-16 17:08:17 +0300

    styled nav

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,21 @@
 </script>
 
 <template>
-  <nav>
-    <div>
-      <RouterLink to="/">Home Page</RouterLink>
-      <RouterLink to="/landing">Landing Page</RouterLink>
-      <RouterLink to="/contact">Contact Page</RouterLink>
-    </div>
+  <nav class="nav">
+    <RouterLink to="/">Home Page</RouterLink>
+    <RouterLink to="/landing">Landing Page</RouterLink>
+    <RouterLink to="/contact">Contact Page</RouterLink>
   </nav>
   <RouterView/>
 </template>
+
+<style scoped>
+  .nav {
+    top: 0;
+    position: absolute;
+    display: flex;
+    background-color: #eee;
+    width: 100%;
+    height: 10vh;
+  }
+</style>
